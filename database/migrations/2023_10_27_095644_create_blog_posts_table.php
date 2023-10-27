f07aaf4db619df4fb793b2847fec06a6f6b22fa5
Avram <avram.tiberiu28@gmail.com>
2023-10-27 13:04:44 +0300

    new file:   app/Http/Controllers/BlogPostController.php 	new file:   database/factories/BlogPostFactory.php 	modified:   database/migrations/2023_10_27_095644_create_blog_posts_table.php

--- database/migrations/2023_10_27_095644_create_blog_posts_table.php
+++ database/migrations/2023_10_27_095644_create_blog_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->id();
+            $table -> text('title');
+            $table -> text('body');
+            $table -> text('user_id');
             $table->timestamps();
         });
     }
