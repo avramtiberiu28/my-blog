ab207120cc217b9ac82be000f2edce37bc9349ad
Avram <avram.tiberiu28@gmail.com>
2023-10-27 13:56:55 +0300

    modified:   app/Http/Controllers/BlogPostController.php 	modified:   app/Models/BlogPost.php 	new file:   resources/views/blog/create.blade.php 	new file:   resources/views/blog/edit.blade.php 	new file:   resources/views/blog/index.blade.php 	new file:   resources/views/blog/show.blade.php

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -9,40 +9,60 @@ class BlogPostController extends Controller
     public function index()
     {
         $posts = BlogPost::all(); //fetch all blog posts from DB
-	    return $posts; //returns the fetched posts
+        return view('blog.index', [
+            'posts' => $posts,
+        ]); //returns the view with posts
     }
 
     public function create()
     {
-        //show form to create a blog post
+        return view('blog.create');
     }
 
    
     public function store(Request $request)
     {
-        //store a new post
+        $newPost = BlogPost::create([
+            'title' => $request->title,
+            'body' => $request->body,
+            'user_id' => 1
+        ]);
+
+        return redirect('blog/' . $newPost->id);
     }
 
     public function show(BlogPost $blogPost)
     {
-        return $blogPost; //returns the fetched posts
+        return view('blog.show', [
+            'post' => $blogPost,
+        ]); //returns the view with the post
     }
 
     
     public function edit(BlogPost $blogPost)
     {
-        //show form to edit the post
+        return view('blog.edit', [
+            'post' => $blogPost,
+        ]); //returns the edit view with the post
     }
 
     
     public function update(Request $request, BlogPost $blogPost)
     {
-        //save the edited post
+        $blogPost->update([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        return redirect('blog/' . $blogPost->id);
     }
+
 
     
     public function destroy(BlogPost $blogPost)
     {
-        //delete a post
+        $blogPost->delete();
+
+        return redirect('/blog');
     }
 }
